fe193e80952b357cc58efc1f5d9c0e19889b6fbc
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-09-11 18:24:10 +0300

    splash view ui done

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -5,6 +6,30 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Spacer(
+          flex: 4,
+        ),
+        Center(
+          child: Image.asset(
+            AssetsData.logo,
+            scale: 2,
+          ),
+        ),
+        const Spacer(
+          flex: 3,
+        ),
+        Text(
+          'improving your mentality.',
+          style: TextStyle(
+            color: Colors.white.withOpacity(0.3),
+            fontSize: 16,
+          ),
+        ),
+        const Spacer(),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,6 +12,12 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp();
+    return GetMaterialApp(
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
+    );
   }
 }
