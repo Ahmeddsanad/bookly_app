2fe2bcc45915663c1af22137a416c4fabe0c14c9
Ahmed Sanad <ahmed.hac26@gmail.com>
2023-09-14 12:24:14 +0300

    best seller list view done

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,76 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 30,
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 120,
+            child: AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    16,
+                  ),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/images/book.png'),
+                  ),
+                ),
+                // child: Image.asset('assets/images/3.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire ',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.textStyle20.copyWith(
+                        fontFamily: 'GT_Sectra',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle14.copyWith(
+                      color: const Color(0xffB7B6BC),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  const BookRating(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/Featured_books_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/Featured_list_view_item.dart';
@@ -13,99 +15,35 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          Padding(
-            padding: EdgeInsets.only(
-              top: 30,
-              left: 30,
-              bottom: 20,
-            ),
-            child: Text(
-              'Best Seller',
-              style: Styles.textStyle18,
-            ),
-          ),
-          BestSellerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 30,
-      ),
-      child: Row(
-        children: [
-          SizedBox(
-            height: 120,
-            child: AspectRatio(
-              aspectRatio: 2.5 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(
-                    16,
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppBar(),
+                FeaturedBooksListView(),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 30,
+                    left: 30,
+                    bottom: 20,
                   ),
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage('assets/images/book.png'),
+                  child: Text(
+                    'Best Seller',
+                    style: Styles.textStyle18,
                   ),
                 ),
-                // child: Image.asset('assets/images/3.png'),
-              ),
+              ],
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 30,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Text(
-                      'Harry Potter and the Goblet of Fire ',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: Styles.textStyle20.copyWith(
-                        fontFamily: 'GT_Sectra',
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Text(
-                    'J.K. Rowling',
-                    style: Styles.textStyle14.copyWith(
-                      color: const Color(0xffB7B6BC),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  BookRating()
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+        ),
+        //SliverToBoxAdapter and put shrikwrap
+        SliverFillRemaining(
+          child: BestSellerListView(),
+        ),
+      ],
     );
   }
 }
